cb23006ff51c9184f0045165ae6b06b266f0b461
Stephen Rivas Jr <sprjr@me.com>
2020-03-22 17:22:58 -0400

    add tailwind trimmings and output css

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+// https://tailwindcss.com/docs/configuration/
+module.exports = {
+    theme: {
+        fontFamily: {
+            display: ['Karla', 'sans-serif'],
+            body: ['Karla', 'sans-serif'],
+            sans: ['Karla', 'sans-serif'],
+            semibold: ['Karla', 'sans-serif'],
+        },
+        extend: {
+            colors: {
+                blue: {
+                    100: '#7BBEE4',
+                    200: '#6AB6E1',
+                    300: '#5AAEDE',
+                    400: '#4AA6DB',
+                    500: '#4497C8',
+                    600: '#3D88B4',
+                    700: '#3679A0',
+                    800: '#306A8C',
+                    900: '#1B4965',
+                }
+            }
+        }
+    }
+}
