834815863bd978f1651989fbafd8d8a74b5812d6
Stephen Rivas Jr <sprjr@me.com>
2020-03-24 16:02:49 -0400

    update config to leverage prod uris when deployed

--- components/auth0/provider.js
+++ components/auth0/provider.js
@@ -56,7 +56,6 @@ const onRedirectCallback = appState => {
 
 const Provider = (props) => {
     const { children } = props;
-
     return <Auth0Provider
         domain={process.env.AUTH0_DOMAIN}
         clientId={process.env.AUTH0_CLIENT_ID}
--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@ const withCSS = require('@zeit/next-css');
 
 module.exports = withCSS({
     env: {
-        API_BASE_URL: 'http://localhost:3001',
+        API_BASE_URL: process.env.API_BASE_URL || 'http://heyanime.show:3001',
         AUTH0_DOMAIN: 'heyanimedotshow.auth0.com',
         AUTH0_CLIENT_ID: '8H2vL22X6kb80ajW8CLQKgUxTPXHbniw',
-        REDIRECT_URI: 'http://localhost:3000/',
-        POST_LOGOUT_REDIRECT_URI: 'http://localhost:3000/'
+        REDIRECT_URI: process.env.REDIRECT_URI || 'http://heyanime.show/',
+        POST_LOGOUT_REDIRECT_URI: process.env.POST_LOGOUT_REDIRECT_URI || 'http://heyanime.show/'
     }
 });
